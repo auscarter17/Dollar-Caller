57f7c158a045dbc1184f2a1af2fa7a55fdb37b44
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2022-06-14 12:40:15 -0400

    added function to submit budget changes when back online

--- public/js/idb.js
+++ public/js/idb.js
@@ -22,3 +22,50 @@ request.onsuccess = function(event) {
 request.onerror = function(event) {
     console.log(event.target.errorCode);
 };
+
+// this function will fire if a budget adjustment is made while offline
+function saveRecord(record) {
+    const transaction = db.transaction(['budget'], 'readwrite');
+    const budgetObjectStore = transaction.budgetObjectStore('budget');
+    budgetObjectStore.add(record);
+}
+
+function uploadBudget() {
+    // open a transaction on db
+    const transaction = db.transaction(['budget'], readwrite);
+
+    //access object store
+    const budgetObjectStore = transaction.ObjectStore('pizza');
+
+    // get records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        // if there was data in idb store, send it to API
+        if (getAll.result.length > 0) {
+            fetch('api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(['budget'], 'readwrite');
+                    const budgetObjectStore = transaction.ObjectStore('budget');
+                    budgetObjectStore.clear();
+
+                    alert('All saved budget changes have been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
